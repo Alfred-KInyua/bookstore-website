835524bf0e646a8d805b64fbba23df3c2baa4965
Kinyua <alfkinyua@gmail.com>
2022-08-11 20:26:17 +0300

    Add book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types';
 
 const Book = (props) => {
-  const { title, author } = props;
+const { title, author } = props;
 
-  return (
-    <div>
-      <p>{title}</p>
-      <p>{author}</p>
-      <button type="button">Delete</button>
-    </div>
-  );
+return (
+<div>
+<p>{title}</p>
+<p>{author}</p>
+<button type="button">Delete</button>
+</div>
+);
 };
 
 Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+title: PropTypes.string.isRequired,
+author: PropTypes.string.isRequired,
 };
 
 export default Book;
