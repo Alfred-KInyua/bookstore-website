0c657e1bc8f58b397ec22d17298a83869e6c951a
Kinyua <alfkinyua@gmail.com>
2022-08-15 21:26:58 +0300

    Add configure store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import booksReducer from "./books/Books";
+import categoriesReducer from "./categories/Categories";
+
+const rootReducer = combineReducers({
+  books: booksReducer,
+  categories: categoriesReducer,
+});
+const store = configureStore({ reducer: rootReducer });
+export default store;
